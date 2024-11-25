c35054a244c6140392d6f66519ab0d80b7da973c
solalohana <solalohana@gmail.com>
2024-11-25 11:00:27 +0200

    engine is now responsible for everything

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import tcod
 
 from engine import Engine
 import entity_factories
-from input_handlers import EventHandler
 from procgen import generate_dungeon
 
 
@@ -23,19 +22,20 @@ def main() -> None:
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
-    event_handler = EventHandler()
-
     player = copy.deepcopy(entity_factories.player)
-    game_map = generate_dungeon(
+
+    engine = Engine(player=player)
+
+    engine.game_map = generate_dungeon(
             max_rooms=max_rooms,
             room_min_size=room_min_size,
             room_max_size=room_max_size,
             map_width=map_width,
             map_height=map_height,
             max_monsters_per_room=max_monsters_per_room,
-            player=player
+            engine=engine,
         )
-    engine = Engine(event_handler=event_handler, game_map=game_map, player=player)
+    engine.update_fov()
 
     with tcod.context.new_terminal(
         screen_width,
@@ -47,9 +47,8 @@ def main() -> None:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
             engine.render(console=root_console, context=context)
-            events = tcod.event.wait()
+            engine.event_handler.handle_events()
 
-            engine.handle_events(events)
 
 
 
